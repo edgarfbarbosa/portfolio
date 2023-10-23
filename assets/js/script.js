570a4e16c2b3e907f695246a4a26d4e11ab41fab
Edgar Faria Barbosa <edgarfbarbosa@outlook.com>
2023-10-23 13:29:53 -0300

    refactor: Encapsula o código referente ao destaque no link do menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,24 @@
-/** Seleciona todos os elementos com a classe 'menu__link'. */
-const menuLinks = document.querySelectorAll('.menu__link')
-
-/** Adiciona a classe 'menu__link--highlight' no primeiro elemento do array-like menuLinks. */
-menuLinks[0].classList.add('menu__link--highlight')
-
-/**
-* Manipula a classe 'menu__link--highlight' nos links dentro de 'menuLinks'.
-* @param {number} index índice do elemento que recebe a classe 'menu__link--highlight'.
-*/
-function handleLinkHighlight(index) {
-  menuLinks.forEach(link => link.classList.remove('menu__link--highlight'))
-  menuLinks[index].classList.add('menu__link--highlight')
+function initMenuAnimation()
+{
+  /** Seleciona todos os elementos com a classe 'menu__link'. */
+  const menuLinks = document.querySelectorAll('.menu__link')
+  
+  /** Adiciona a classe 'menu__link--highlight' no primeiro elemento do array-like menuLinks. */
+  menuLinks[0].classList.add('menu__link--highlight')
+  
+  /**
+  * Manipula a classe 'menu__link--highlight' nos links dentro de 'menuLinks'.
+  * @param {number} index índice do elemento que recebe a classe 'menu__link--highlight'.
+  */
+  function handleLinkHighlight(index) {
+    menuLinks.forEach(link => link.classList.remove('menu__link--highlight'))
+    menuLinks[index].classList.add('menu__link--highlight')
+  }
+  
+  /** Adiciona um evento de 'click' a cada link, chamando a função 'handleLinkHighlight(index)' ao passar o mouse por cima. */
+  menuLinks.forEach((link, index) => {
+    link.addEventListener('click', () => {handleLinkHighlight(index)})
+  })
 }
 
-/** Adiciona um evento de 'click' a cada link, chamando a função 'handleLinkHighlight(index)' ao passar o mouse por cima. */
-menuLinks.forEach((link, index) => {
-  link.addEventListener('click', () => {handleLinkHighlight(index)})
-})
+initMenuAnimation()
